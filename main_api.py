cac964ceca5b60a3d8cf73f9c7c0fcd40f5429d8
Le Van Hung <hailuacx@gmail.com>
2026-07-08 15:46:27 +0700

    Them domain Vercel vao CORS

--- main_api.py
+++ main_api.py
@@ -78,7 +78,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",   # React dev server
         "http://localhost:5173",   # Vite dev server
-        "https://truonghoc.com"
+        "https://quanlytruonghoc-amber.vercel.app/login"
     ],
     allow_credentials=True,
     allow_methods=["*"],
