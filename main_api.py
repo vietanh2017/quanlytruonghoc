8d690689452d03ecee28c8337d2a0ea084c80370
Le Van Hung <hailuacx@gmail.com>
2026-07-08 16:00:17 +0700

    Sua dung domain CORS, bo /login thua

--- main_api.py
+++ main_api.py
@@ -78,7 +78,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",   # React dev server
         "http://localhost:5173",   # Vite dev server
-        "https://quanlytruonghoc-amber.vercel.app/login"
+        "https://quanlytruonghoc-amber.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
